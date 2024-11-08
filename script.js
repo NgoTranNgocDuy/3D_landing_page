1c21b65d78d8933dedb1398f50446e7d4d20c76b
NgocDuy1234 <tngo0031@student.monash.edu>
2024-11-08 15:29:57 +1100

    Add tech urls

--- script.js
+++ script.js
@@ -13,7 +13,15 @@ const colors = [
 ];
 
 const techs = ["JS", "React", "Vue", "Angular", "Node", "Sass", "Ember"];
-
+const techUrls = {
+  JS: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  React: "https://reactjs.org/",
+  Vue: "https://vuejs.org/",
+  Angular: "https://angular.io/",
+  Node: "https://nodejs.org/",
+  Sass: "https://sass-lang.com/",
+  Ember: "https://emberjs.com/"
+};
 let current = 1;
 
 const textStyle = () => {
@@ -22,6 +30,14 @@ const textStyle = () => {
   btn.style.backgroundColor = colors[current - 1];
   btn.firstElementChild.textContent = techs[current - 1];
 };
+// update button with correct urls
+btn.addEventListener("click", () => {
+  const techName = techs[current - 1];
+  const url = techUrls[techName];
+  if (url) {
+    window.location.href = url;
+  }
+});
 
 let interval = setInterval(() => {
   boxes.forEach((box) => {
